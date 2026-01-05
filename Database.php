69f3c3f6eb7e4fa3cdbb9cc33b8c1d5fc73c693d
Vitaliy Yurkin <68192985+Vit0607@users.noreply.github.com>
2026-01-05 21:38:32 +0300

    method query

--- Database.php
+++ Database.php
@@ -2,12 +2,11 @@
 
 class Database {
     private static $instance = null;
-    private $pdo;
+    private $pdo, $query, $error = false, $results, $count;
 
     private function __construct() {
         try {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=php-crud-basic', 'root', '');
-            echo 'Ok';
+            $this->pdo = new PDO('mysql:host=localhost;dbname=php-oop', 'root', '');
         } catch(PDOException $exception) {
             die($exception->getMessage());
         }
@@ -19,5 +18,39 @@ class Database {
         }
 
         return self::$instance;
+    }
+
+    public function query($sql, $params = []) {
+        $this->error = false;
+        $this->query = $this->pdo->prepare($sql);
+
+        if (count($params)) {
+            $i = 1;
+            foreach($params as $param) {
+                $this->query->bindValue($i, $param);
+                $i++;
+            }
+        }
+
+        if (!$this->query->execute()) {
+            $this->error = true;
+        } else {
+            $this->results = $this->query->fetchAll(PDO::FETCH_OBJ);
+            $this->count = $this->query->rowCount();
+        }
+
+        return $this;
+    }
+
+    public function error() {
+        return $this->error;
+    }
+
+    public function results() {
+        return $this->results;
+    }
+
+    public function count() {
+        return $this->count;
     }
 }
--- index.php
+++ index.php
@@ -2,5 +2,16 @@
 
 require_once 'Database.php';
 
-Database::getInstance();
-Database::getInstance();
+$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['Alita Gray', 'Jim Ketty']);
+
+if ($users->error()) {
+    echo 'we have an error';
+} else {
+    foreach ($users->results() as $user) {
+        echo $user->username . '<br>';
+    }
+}
+
+if ($users->count()) {
+    echo $users->count();
+}
