bd73b6159d2e82717f92a63cf1c58162d5c1e6a0
Vitaliy Yurkin <68192985+Vit0607@users.noreply.github.com>
2026-01-09 22:54:37 +0300

    component Config

--- Database.php
+++ Database.php
@@ -6,7 +6,7 @@ class Database {
 
     private function __construct() {
         try {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=php-oop', 'root', '');
+            $this->pdo = new PDO("mysql:host=" . Config::get('mysql.host') . ";dbname=" . Config::get('mysql.database'), Config::get('mysql.username'), Config::get('mysql.password'));
         } catch(PDOException $exception) {
             die($exception->getMessage());
         }
--- index.php
+++ index.php
@@ -1,37 +1,21 @@
 <?php
 
 require_once 'Database.php';
+require_once 'Config.php';
 
-// $users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['Alita Gray', 'Jim Ketty']);
-$users = Database::getInstance()->get('users', ['username', '=', 'orange']);
-// $users = Database::getInstance()->delete('users', ['id', '=', '13']);
+$GLOBALS['config'] = 
+[
+    'mysql' => [
+        'host' => 'localhost',
+        'database' => 'php-oop',
+        'username' => 'root',
+        'password' => '',
+        'something' => [
+            'no' => [
+                'bar' => 'baz'
+            ]
+        ]
+    ]
+];
 
-// Database::getInstance()->insert('users', [
-//     'username' => 'orange',
-//     'password' => '123',
-//     'email' => 'b@gmail.com'
-// ]);
-
-Database::getInstance()->update('users', 15, [
-    'username' => 'orange2',
-    'password' => '1234',
-    'email' => 'bb@gmail.com'
-]);
-
-echo $users->first()->email;
-
-echo '<br>';
-echo '<br>';
-echo '<br>';
-
-if ($users->error()) {
-    echo 'we have an error';
-} else {
-    foreach ($users->results() as $user) {
-        echo $user->username . '<br>';
-    }
-}
-
-if ($users->count()) {
-    echo $users->count();
-}
+echo Config::get('mysql.something.no.bar');
