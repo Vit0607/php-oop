d3ddcad1a2a62a39f4a293974c46be6ffbc93bb0
Vitaliy Yurkin <68192985+Vit0607@users.noreply.github.com>
2026-01-12 11:28:38 +0300

    components Validation and Input

---
+++ Input.php
@@ -0,0 +1,25 @@
+<?php
+
+class Input {
+    public static function exists($type = 'post') {
+        switch($type) {
+            case 'post':
+                return (!empty($_POST)) ? true : false; 
+            case 'get':
+                return (!empty($_POST)) ? true : false;
+            default:
+                return false;
+            break;
+        }
+    }
+
+    public static function get($item) {
+        if (isset($_POST[$item])) {
+            return $_POST[$item];
+        } elseif (isset($_GET[$item])) {
+            return $_GET[$item];
+        }
+        
+       return ''; 
+    }
+}
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once 'Database.php';
 require_once 'Config.php';
+require_once 'Input.php';
+require_once 'Validate.php';
 
 $GLOBALS['config'] = 
 [
@@ -18,4 +20,51 @@ $GLOBALS['config'] =
     ]
 ];
 
-echo Config::get('mysql.something.no.bar');
+if (Input::exists()) {
+    $validate = new Validate();
+
+    $validation = $validate->check($_POST, [
+        'username' => [
+            'required' => true,
+            'min' => 2,
+            'max' => 15,
+            'unique' => 'users'
+        ],
+        'password' => [
+            'required' => true,
+            'min' => 3
+        ],
+        'password_again' => [
+            'required' => true,
+            'matches' => 'password'
+        ]
+    ]);
+
+    if ($validation->passed()) {
+        echo 'passed';
+    } else {
+        foreach ($validation->errors() as $error) {
+            echo $error . '<br>';
+        }
+    }
+}
+
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" value="<?php echo Input::get('username'); ?>" />
+    </div>
+    <div class="field">
+        <label for="password">Password</label>
+        <input type="text" name="password" />
+    </div>
+    <div class="field">
+        <label for="password_again">Password Again</label>
+        <input type="text" name="password_again" />
+    </div>
+    <div class="field">
+        <button type="text">Submit</button>
+    </div>
+</form>
